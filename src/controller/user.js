96d7ae4d067b8079a194ad16fd756174a6c917a3
Jeff <982855239@qq.com>
2020-05-25 14:01:00 +0800

    md5加密

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,7 +1,12 @@
 const { exec,escape } = require("../db/mysql");
+const {genPassword} = require('../utils/cryp');
 const login = (username, password) => {
   username = escape(username);
-  password = escape(password)
+
+  //生成加密密码
+  password = genPassword(password);
+  password = escape(password);
+
   const sql = `
     select username, realname from users where username=${username} and password=${password};
   `;
