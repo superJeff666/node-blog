f1f7f9cc23850bbb1dede6b36b826d43a5b8dc9d
Jeff <982855239@qq.com>
2020-05-26 14:11:12 +0800

    koa2写日志

--- blog-koa2/app.js
+++ blog-koa2/app.js
@@ -7,6 +7,9 @@ const bodyparser = require("koa-bodyparser");
 const logger = require("koa-logger");
 const session = require("koa-generic-session");
 const redisStore = require("koa-redis");
+const path = require('path');
+const fs = require('fs');
+const morgan = require('koa-morgan');
 
 const index = require("./routes/index");
 const users = require("./routes/users");
@@ -40,6 +43,21 @@ app.use(async (ctx, next) => {
   const ms = new Date() - start;
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 });
+
+const ENV = process.env.NODE_ENV;
+if (ENV !== "production") {
+  app.use(morgan("dev"));
+} else {
+  const logFileName = path.join(__dirname, "logs", "access.log");
+  const writeStream = fs.createWriteStream(logFileName, {
+    flags: "a"
+  });
+  app.use(
+    morgan("combined", {
+      stream: writeStream
+    })
+  );
+}
 
 //session 配置
 app.keys = ["jawJiOl#987"];
